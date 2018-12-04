e87a3dd6085e7b69f490c666a2b715c752205c9c
Lei_tiger <leifu@oregonstate.edu>
2018-12-03 16:55:52 -0800

    modified day1

--- day1/day1.js
+++ day1/day1.js
@@ -1,62 +1,40 @@
 const fs=require('fs')
 
 // // --- Part one ---
+//reading data from file day1.txt
 fs.readFile('./day1.txt',(err,data)=>{
 	console.time('funccccc')
+	//if there is an error, throw the following error
 	if (err) {
 		console.log('tttttttigerrrrr');
 	}
-
 	dataArray=data.toString().split('\n')
+
+//---part one---
 	const frequency = dataArray.reduce((acc,currentValue)=>{
-		let currentSign=currentValue[0]
-		let currentNum=Number(currentValue.slice(1,currentValue.length));
+		currentValue=Number(currentValue);
+		return acc +=currentValue;},0)
+    console.log('solution for part 1: frequency is ',frequency)
 
-		if (currentSign=="+") {
-			return acc += currentNum;
-		} else {
-			return acc -= currentNum;
-		}
-	},0)
-    console.log('frequency',frequency)
-	console.timeEnd('funccccc')
-})
-
-//-----solutin for part one is 439
-
-
-// --- Part two ---
-fs.readFile('./day1.txt',(err,data)=>{
-	if (err) {
-		console.log('tttttttigerrrrr');
-	}
-
-	dataArray=data.toString().split('\n')
-	let frequencyReached=[];
+//---part two---
+    let frequencyReached=[];
 	let returnStatus = true;
 	let acc = 0;
-
+	//while returnStatus is true, it will nonstop loop
 	while (returnStatus) {
-		for (var i = 0; i<dataArray.length;i++) {
-			let currentSign=dataArray[i][0]
-			let currentNum=Number(dataArray[i].slice(1,dataArray.length));
-
-			if (currentSign == "+") {
-				currentValue = currentNum;
-				acc += currentValue;
+		dataArray.forEach (item=>{
+			currentValue=Number(item);
+			acc += currentValue;
+			//if find the repeated frequency, get out of the while loop by changing the returnStatus
+			if(frequencyReached.includes(acc)&&returnStatus==true) {
+				console.log('Solution for part two: frequency repeated is ',acc);
+				return returnStatus = false;
 			} else {
-				currentValue=-currentNum;
-				acc += currentValue;}
-
-			for (var j=1;j<frequencyReached.length;j++) {
-				if (acc === frequencyReached[j]){
-					console.log('repeat',acc);
-					return returnStatus=false;
-				} 
+				frequencyReached.push(acc);
 			}
-			frequencyReached.push(acc);
 		}
-	}
+	)}
 })
 
+//----solutin for part one is 439
 //----solution for part two is 124645
